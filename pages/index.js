d17df2c9eecf640a247e466ae84e1b785b448ccf
Pouyan Madanipour <pouyan.mad@gmail.com>
2023-08-08 09:21:51 +0330

    updaye: home page

--- pages/index.js
+++ pages/index.js
@@ -9,11 +9,11 @@ export default function Home() {
   const { menuMobile, setMenuMobile } = useContext(StateContext);
 
   return (
-    <div className={classes.main}>
+    <Fragment>
       <div className={classes.menu}>
         <Menu />
       </div>
-      <div className={classes.backgroundCover}>
+      <div className={classes.container}>
         <Image
           className={classes.image}
           src={background}
@@ -24,26 +24,26 @@ export default function Home() {
           priority
           loading="eager"
         />
-        <div className={classes.ctaContainer}>
-          <p>نگهبان زندگی</p>
+        <div className={classes.header}>
+          <h1>نگهبان زندگی</h1>
           <button>رزرو وقت حضوری</button>
-          <button className={classes.margin}>مشاور آنلاین رایگان</button>
+          <button>مشاور آنلاین رایگان</button>
         </div>
         <div className={classes.highlightContainer}>
-          <div className={classes.highlightThree}>asdas</div>
-          <div className={classes.highlightTwo}>asdasd</div>
           <div className={classes.highlightOne}>asddasd</div>
+          <div className={classes.highlightTwo}>asdasd</div>
+          <div className={classes.highlightThree}>asdas</div>
+        </div>
+        <div
+          className={`${classes.heading} animate__animated animate__fadeInLeft`}
+        >
+          <h1>Find clarity in chaos</h1>
+          <p>
+            Major Tom is a full-service marketing agency been purpose-built to
+            help organizations thrive in an increasingly complex landscape.
+          </p>
         </div>
       </div>
-      {/* <div
-        className={`${classes.heading} animate__animated animate__fadeInLeft`}
-      >
-        <h1>Find clarity in chaos</h1>
-        <p>
-          Major Tom is a full-service marketing agency been purpose-built to
-          help organizations thrive in an increasingly complex landscape.
-        </p>
-      </div> */}
-    </div>
+    </Fragment>
   );
 }
