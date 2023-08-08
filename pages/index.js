df652929985cbc86b46fbab11455cd2c028a52af
Pouyan Madanipour <pouyan.mad@gmail.com>
2023-08-08 09:51:50 +0330

    update: home page

--- pages/index.js
+++ pages/index.js
@@ -2,18 +2,16 @@ import { useState, useContext, useRef, Fragment, useEffect } from "react";
 import { StateContext } from "../context/stateContext";
 import classes from "./home.module.scss";
 import Menu from "@/components/Menu";
-import Image from "next/image";
+import Image from "next/legacy/image";
 import background from "../assets/background.jpg";
 
 export default function Home() {
-  const { menuMobile, setMenuMobile } = useContext(StateContext);
-
   return (
     <Fragment>
       <div className={classes.menu}>
         <Menu />
       </div>
-      <div className={classes.container}>
+      <div className={classes.heroHeader}>
         <Image
           className={classes.image}
           src={background}
@@ -29,20 +27,20 @@ export default function Home() {
           <button>رزرو وقت حضوری</button>
           <button>مشاور آنلاین رایگان</button>
         </div>
-        <div className={classes.highlightContainer}>
-          <div className={classes.highlightOne}>asddasd</div>
-          <div className={classes.highlightTwo}>asdasd</div>
-          <div className={classes.highlightThree}>asdas</div>
-        </div>
-        <div
-          className={`${classes.heading} animate__animated animate__fadeInLeft`}
-        >
-          <h1>Find clarity in chaos</h1>
-          <p>
-            Major Tom is a full-service marketing agency been purpose-built to
-            help organizations thrive in an increasingly complex landscape.
-          </p>
-        </div>
+      </div>
+      <div className={classes.highlightContainer}>
+        <div className={classes.highlightOne}></div>
+        <div className={classes.highlightTwo}></div>
+        <div className={classes.highlightThree}></div>
+      </div>
+      <div
+        className={`${classes.heading} animate__animated animate__fadeInLeft`}
+      >
+        <h1>Find clarity in chaos</h1>
+        <p>
+          Major Tom is a full-service marketing agency been purpose-built to
+          help organizations thrive in an increasingly complex landscape.
+        </p>
       </div>
     </Fragment>
   );
