3347f98f71e7c3739c8238a233c9999457d963c6
Pouyan Madanipour <pouyan.mad@gmail.com>
2023-08-26 15:04:31 +0330

    update: progressbar

---
+++ components/Progress.js
@@ -0,0 +1,25 @@
+export default function Progress(props) {
+  const { color, completed } = props;
+
+  const progress = {
+    height: 5,
+    width: "100%",
+    backgroundColor: "#ffffff",
+    borderRadius: 50,
+  };
+
+  const filler = {
+    height: "100%",
+    width: `${completed}%`,
+    backgroundColor: color,
+    transition: "width 1s ease-in-out",
+    borderRadius: "inherit",
+    textAlign: "right",
+  };
+
+  return (
+    <div style={progress}>
+      <div style={filler}></div>
+    </div>
+  );
+}
--- pages/assessment/index.js
+++ pages/assessment/index.js
@@ -1,5 +1,25 @@
+import { useState, useContext, Fragment, useEffect } from "react";
 import classes from "./assessment.module.scss";
+import Progress from "@/components/Progress";
 
 export default function Assessment() {
-  return <div className={classes.container}>Assessment</div>;
+  const [progressCompleted, setProgressCompleted] = useState(20);
+
+  return (
+    <div className={classes.container}>
+      <Progress color={"#e7c69a"} completed={progressCompleted} />
+      <button
+        disabled={progressCompleted === 0}
+        onClick={() => setProgressCompleted(progressCompleted - 20)}
+      >
+        back
+      </button>
+      <button
+        disabled={progressCompleted === 100}
+        onClick={() => setProgressCompleted(progressCompleted + 20)}
+      >
+        next
+      </button>
+    </div>
+  );
 }
