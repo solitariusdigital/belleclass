a76ed2a7a992073e51182d5e32afa150f3f0bdaa
Pouyan Madanipour <pouyan.mad@gmail.com>
2023-08-26 17:34:25 +0330

    feat: scroll to top

--- pages/expertises/index.js
+++ pages/expertises/index.js
@@ -3,6 +3,7 @@ import { StateContext } from "../../context/stateContext";
 import classes from "./expertise.module.scss";
 import Expertise from "@/components/Expertise";
 import FiberManualRecordOutlined from "@mui/icons-material/FiberManualRecordOutlined";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 
 export default function Expertises() {
   const { expertiseAreas, setExpertiseAreas } = useContext(StateContext);
@@ -846,6 +847,11 @@ export default function Expertises() {
           )}
         </div>
       )}
+      <KeyboardArrowUpIcon
+        className="icon"
+        onClick={() => window.scrollTo(0, 0)}
+        sx={{ fontSize: 36 }}
+      />
     </div>
   );
 }
