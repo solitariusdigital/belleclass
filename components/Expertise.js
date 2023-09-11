5a7aa97b0d378ecb4154cc74ed55984e1b68394f
Pouyan Madanipour <pouyan.mad@gmail.com>
2023-09-11 17:39:26 +0330

    update: images

--- components/Expertise.js
+++ components/Expertise.js
@@ -3,10 +3,66 @@ import { StateContext } from "@/context/stateContext";
 import classes from "./Expertise.module.scss";
 import Router from "next/router";
 
+import Belle33 from "@/assets/Belle-33.png";
+import Belle34 from "@/assets/Belle-34.png";
+import Belle31 from "@/assets/Belle-31.png";
+import Belle38 from "@/assets/Belle-38.png";
+import Belle29 from "@/assets/Belle-29.png";
+import Belle30 from "@/assets/Belle-30.png";
+import Belle32 from "@/assets/Belle-32.png";
+import Belle36 from "@/assets/Belle-36.png";
+import Belle35 from "@/assets/Belle-35.png";
+import Belle37 from "@/assets/Belle-37.png";
+
+import Image from "next/legacy/image";
+
 export default function Expertise() {
   const { expertiseAreas, setExpertiseAreas } = useContext(StateContext);
   const { navigationTopBar, setNavigationTopBar } = useContext(StateContext);
   const { displayExpertise, setDisplayExpertise } = useContext(StateContext);
+
+  const images = [
+    {
+      title: "فیلر",
+      image: Belle33,
+    },
+    {
+      title: "نوتریژنومیکس",
+      image: Belle34,
+    },
+    {
+      title: "لیزر",
+      image: Belle31,
+    },
+    {
+      title: "کاشت مو و ابرو",
+      image: Belle38,
+    },
+    {
+      title: "بوتاکس",
+      image: Belle29,
+    },
+    {
+      title: "پلاسمای غنی از پلاکت",
+      image: Belle30,
+    },
+    {
+      title: "درمان با فرکانس رادویی",
+      image: Belle32,
+    },
+    {
+      title: "هایفوتراپی",
+      image: Belle36,
+    },
+    {
+      title: "کرایوتراپی",
+      image: Belle37,
+    },
+    {
+      title: "لیفت با نخ",
+      image: Belle35,
+    },
+  ];
 
   const expertisesPage = (title) => {
     let route = "/expertises";
@@ -28,13 +84,19 @@ export default function Expertise() {
   return (
     <div className={classes.items}>
       {expertiseAreas.map((item, index) => (
-        <p
+        <div
           className={item.active ? classes.active : ""}
           key={index}
           onClick={() => expertisesPage(item.title)}
         >
-          {item.title}
-        </p>
+          <Image
+            width={150}
+            height={150}
+            src={images[index].image}
+            alt="logo"
+          />
+          <p>{item.title}</p>
+        </div>
       ))}
     </div>
   );
